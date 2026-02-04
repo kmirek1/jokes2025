1756770a83147fa7d05fdab029c0ded51c917f89
kmirek1 <karissa.mirek@gmail.com>
2026-02-04 20:52:24 +0000

    changes

--- main.py
+++ main.py
@@ -4,55 +4,52 @@
 # make this performance task ready for submission
 # To give the user a fun experience hearing knock knock jokes
 
-
-    
-
-
+ #this function allows the game to start and 
 def start():
-     answer = input("Do you want to hear a joke? ").lower()
+     # Added a name parameter to pass through the game
+     name = input("What is your name? ")
+     answer = input(f"Hi {name}, do you want to hear a joke? ").lower()
      if answer == "no":
         print("Okay suit yourself!")
      elif answer == "yes":
         print("Great, let's play!")
-        joke_topic()
-
-def rating():
-    rate = int(input("Please rate our game 1-10! "))
-    print(f"User Satisifaction: {rate * 10}%")
+        joke_topic(name)
+#this function lets the user pick what topic the joke will be about
+def joke_topic(user_name):
+     topics = ["robbers", "tanks", "pencils"]
+     print(f"\nOkay {user_name}, we have: {', '.join(topics)}")
+     topic = input("Which one do you choose? ").lower()
+     #the jokes are listed here 
+     if topic == "robbers":
+        input("Knock Knock... (Press Enter)")
+        input("Calder... ")
+        print("Calder police - I've been robbed!")
+     elif topic == "tanks":
+        input("Knock Knock... ")
+        input("Tank... ")
+        print("You're welcome!")
+     elif topic == "pencils":
+        input("Knock Knock... ")
+        input("Broken pencil... ")
+        print("Nevermind, it's pointless!")
+     else:
+        print("Sorry, I'm not funny enough to know that one...")
+        return joke_topic(user_name)
+     exit_game(user_name)
+#this function lets the user rate the game once they are done listening to jokes
+def rating(user_name):
+    rate = int(input(f"Please rate our game 1-10, {user_name}! "))
+    print(f"User Satisfaction: {rate * 10}%")
     if rate > 6:
         print("Glad you liked it!")
     else:
-        print("Sorry you didn't like")
-
-
-
-
-def joke_topic():
-     topics = ["robbers", "tanks", "pencils" ]
-     topic = input("Do you want to hear a joke about robbers, tanks, or pencils?").lower()
-     
-     if topic == "robbers":
-        input("Knock Knock ")
-        input("Calder")
-        print("Calder police - I've been robbed!")
-     elif topic == "tanks":
-        input("Knock Knock ")
-        input("Tank ")
-        input("You are welcome! ")
-     elif topic == "pencils":
-        input("Knock Knock ")
-        input("Broken pencil ")
-        input("Nevermind, it's pointless! ")
-     else:
-        print("Sorry I'm not funny enough to know that...")
-        return joke_topic()
-     
-     exit()
-
-def exit():
-     joke = input("Do you want to hear another joke? ").lower()
+        print("Sorry you didn't like it.")
+#this function ends the game once the user is finished 
+def exit_game(user_name):
+     joke = input(f"\n{user_name}, do you want to hear another joke? ").lower()
      if joke == "no":
-          rating()
+          rating(user_name)
      else:
-        joke_topic()
+        joke_topic(user_name)
+
 start()
